5e63f28be8455285034c7177bb7ab25891d5c1f5
YuMoua <153407360+YuMoua@users.noreply.github.com>
2024-03-06 20:52:51 -0600

    updated

--- assets/script.js
+++ assets/script.js
@@ -13,6 +13,7 @@ let cityNameSearched = document.getElementById('cityNameSearched');
 
 
 // Search Bar Button Element
+let searchForm = document.getElementById('searchForm');
 let citySearchBarBTN = document.getElementById('citySearchBarBTN');
 
 
@@ -30,57 +31,189 @@ let sixthDay = document.getElementById('sixthDay');
 // Weather API KEY
 let apiKey = 'dddef569111da5d78cc08aff79646497';
 
+
 // Weather Forcast API to find weather
-let weatherAPIURL = 'http//api.openweathermap.org/data/2.5/forecast?lat=' + lattitude + '&lon=' + longitude + '&appid=' + apiKey;
 
 let requestOptions = {
     method: 'GET',
+    mode: 'no-cors',
     redirect: 'follow'
-
 }
 
-function weatherGet(weatherAPIURL) {
-    fetch(weatherAPIURL, requestOptions)
+function weatherGet(cityName) {
+    
+    let weatherAPIURL = 'https://api.openweathermap.org/data/2.5/forecast?q=' + cityName + '&appid=' + apiKey + '&units=imperial';
+    fetch(weatherAPIURL)
     .then(response => response.json())
-}
-// Geocoding API to find coordinates
-let geocodingAPI = 'http://api.openweathermap.org/geo/1.0/direct?q=' + cityName + '&limit=1&appid=' + apiID;
+    .then((data) => {
+        console.log(data)
+        console.log(data.list)
+        console.log(data.list[0])
+        console.log(data.list[0].main)
+    })
+    .catch(error => console.log('Error', error))
+    
+};
 
 // Button EventListeners
-citySearchBarBTN.addEventListener('click', function() {
+searchForm.addEventListener('submit', function(event) {
     // Need to add function to input city name into the API.
-    
+    event.preventDefault();
+    let cityName = cityNameSearched.value.trim()
+    console.log(cityName);
+    weatherGet(cityName)
+
 
 });
 
-atlantaWeatherBTN.addEventListener('click', function() {
+atlantaWeatherBTN.addEventListener('click', function () {
     // Need to add function to search city weather in the API
+    let latitude = '33.7488';
+    let longitude = '84.3877';
+    function weatherGet(weatherAPIURL, response) {
+        let weatherAPIURL = 'https//api.openweathermap.org/data/2.5/forecast?lat=' + latitude + '&lon=' + longitude + '&appid=' + apiKey;
+        let requestOptions = {
+            'method': 'GET',
+            'mode': 'no-cors',
+            'redirect': 'follow'
+        }
+        fetch(weatherAPIURL, requestOptions)
+            .then(response => response.json())
+        console.log(response);
+    };
+
+    weatherGet()
 });
 
-denverWeatherBTN.addEventListener('click', function() {
+denverWeatherBTN.addEventListener('click', function () {
     // Need to add function to search city weather in the API
+    let latitude = '39.7392';
+    let longitude = '104.9903';
+    function weatherGet(weatherAPIURL, response) {
+        let weatherAPIURL = 'https//api.openweathermap.org/data/2.5/forecast?lat=' + latitude + '&lon=' + longitude + '&appid=' + apiKey;
+        let requestOptions = {
+            'method': 'GET',
+            'mode': 'no-cors',
+            'redirect': 'follow'
+        }
+        fetch(weatherAPIURL, requestOptions)
+            .then(response => response.json())
+        console.log(response);
+    };
+
+    weatherGet()
 });
 
-seattleWeatherBTN.addEventListener('click', function() {
+seattleWeatherBTN.addEventListener('click', function () {
     // Need to add function to search city weather in the API
+    let latitude = '47.6061';
+    let longitude = '122.3328';
+    function weatherGet(weatherAPIURL, response) {
+        let weatherAPIURL = 'https//api.openweathermap.org/data/2.5/forecast?lat=' + latitude + '&lon=' + longitude + '&appid=' + apiKey;
+        let requestOptions = {
+            'method': 'GET',
+            'mode': 'no-cors',
+            'redirect': 'follow'
+        }
+        fetch(weatherAPIURL, requestOptions)
+            .then(response => response.json())
+        console.log(response);
+    };
+
+    weatherGet()
 });
 
-sanFranciscoWeatherBTN.addEventListener('click', function() {
+sanFranciscoWeatherBTN.addEventListener('click', function () {
     // Need to add function to search city weather in the API
+    let latitude = '37.7749';
+    let longitude = '122.4194';
+    function weatherGet(weatherAPIURL, response) {
+        let weatherAPIURL = 'https//api.openweathermap.org/data/2.5/forecast?lat=' + latitude + '&lon=' + longitude + '&appid=' + apiKey;
+        let requestOptions = {
+            'method': 'GET',
+            'mode': 'no-cors',
+            'redirect': 'follow'
+        }
+        fetch(weatherAPIURL, requestOptions)
+            .then(response => response.json())
+        console.log(response);
+    };
+
+    weatherGet()
 });
 
-orlandoWeatherBTN.addEventListener('click', function() {
+orlandoWeatherBTN.addEventListener('click', function () {
     // Need to add function to search city weather in the API
+    let latitude = '28.5384';
+    let longitude = '81.3789';
+    function weatherGet(weatherAPIURL, response) {
+        let weatherAPIURL = 'https//api.openweathermap.org/data/2.5/forecast?lat=' + latitude + '&lon=' + longitude + '&appid=' + apiKey;
+        let requestOptions = {
+            'method': 'GET',
+            'mode': 'no-cors',
+            'redirect': 'follow'
+        }
+        fetch(weatherAPIURL, requestOptions)
+            .then(response => response.json())
+        console.log(response);
+    };
+
+    weatherGet()
 });
 
-newYorkWeatherBTN.addEventListener('click', function() {
+newYorkWeatherBTN.addEventListener('click', function () {
     // Need to add function to search city weather in the API
+    let latitude = '40.7128';
+    let longitude = '74.0060';
+    function weatherGet(weatherAPIURL, response) {
+        let weatherAPIURL = 'https//api.openweathermap.org/data/2.5/forecast?lat=' + latitude + '&lon=' + longitude + '&appid=' + apiKey;
+        let requestOptions = {
+            'method': 'GET',
+            'mode': 'no-cors',
+            'redirect': 'follow'
+        }
+        fetch(weatherAPIURL, requestOptions)
+            .then(response => response.json())
+        console.log(response);
+    };
+
+    weatherGet()
 });
 
-chicagoWeatherBTN.addEventListener('click', function() {
+chicagoWeatherBTN.addEventListener('click', function () {
     // Need to add function to search city weather in the API
+    let latitude = '41.8781';
+    let longitude = '87.6298';
+    function weatherGet(weatherAPIURL, response) {
+        let weatherAPIURL = 'https//api.openweathermap.org/data/2.5/forecast?lat=' + latitude + '&lon=' + longitude + '&appid=' + apiKey;
+        let requestOptions = {
+            'method': 'GET',
+            'mode': 'no-cors',
+            'redirect': 'follow'
+        }
+        fetch(weatherAPIURL, requestOptions)
+            .then(response => response.json())
+        console.log(response);
+    };
+
+    weatherGet()
 });
 
-austinWeatherBTN.addEventListener('click', function() {
+austinWeatherBTN.addEventListener('click', function () {
     // Need to add function to search city weather in the API
+    let latitude = '30.2672';
+    let longitude = '97.7431';
+    function weatherGet(weatherAPIURL, response) {
+        let weatherAPIURL = 'https//api.openweathermap.org/data/2.5/forecast?lat=' + latitude + '&lon=' + longitude + '&appid=' + apiKey;
+        let requestOptions = {
+            'method': 'GET',
+            'mode': 'no-cors',
+            'redirect': 'follow'
+        }
+        fetch(weatherAPIURL, requestOptions)
+            .then(response => response.json())
+        console.log(response);
+    };
+
+    weatherGet()
 });
